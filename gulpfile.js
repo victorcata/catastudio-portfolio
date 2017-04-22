3b029008912643f1d61c1188b550be9e2a9c260f
Victor Cata <catarsis2001@hotmail.com>
2017-04-22 22:15:17 +0200

    sass modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
-    sourcemaps = require("gulp-sourcemaps");
+    sourcemaps = require("gulp-sourcemaps"),
+    bulkSass = require('gulp-sass-bulk-import');
 
 gulp.task("sass", function() {
     return gulp.src("./styles/app.scss")
                .pipe(sourcemaps.init())
+               .pipe(bulkSass())
                .pipe(sass().on("error", sass.logError))
                .pipe(sourcemaps.write())
-               .pipe(gulp.dest("./styles/app.css"));
+               .pipe(gulp.dest("./styles"));
 });
 
 gulp.task("sass:watch", function() {
     gulp.watch("./styles/**/*.scss", ["sass"]);
 });
 
-gulp.task("default",["sass", "sass:watch"]);
+gulp.task("default",["sass"]);
