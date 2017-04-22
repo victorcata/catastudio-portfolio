2f12b497ebcd2881eedcff84a1577dda69d7f770
Victor Cata <catarsis2001@hotmail.com>
2017-04-23 01:06:59 +0200

    pug templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
     sourcemaps = require("gulp-sourcemaps"),
-    bulkSass = require('gulp-sass-bulk-import');
+    bulkSass = require('gulp-sass-bulk-import'),
+    pug = require("gulp-pug");
 
+/**
+ * SASS
+ */
 gulp.task("sass", function() {
     return gulp.src("./styles/app.scss")
                .pipe(sourcemaps.init())
@@ -12,8 +16,21 @@ gulp.task("sass", function() {
                .pipe(gulp.dest("./styles"));
 });
 
+/**
+ * SASS Watcher
+ */
 gulp.task("sass:watch", function() {
     gulp.watch("./styles/**/*.scss", ["sass"]);
 });
 
-gulp.task("default",["sass"]);
+/**
+ * PUG
+ */
+gulp.task("pug", function() {
+    return gulp.src("./templates/index.pug")
+               .pipe(pug())
+               .pipe(gulp.dest("."));
+});
+
+
+gulp.task("default", ["sass", "pug"]);
